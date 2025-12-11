753cd23c6008b54d10365169361be94056d125c3
traviscloke126 <traviscloke126@gmail.com>
2025-12-11 02:21:12 -0600

    loops

--- app.js
+++ app.js
@@ -11,14 +11,22 @@
 //}
 
 
-for (let i = 1; i <= 20; ++i) {
-    if (i % 3 === 0 && i % 5 === 0)
-        console.log(`${i}->Frontend Simplified`)
-    else if (i % 3 === 0)
-        console.log(`${i}->Frontend`)
-    else if (i % 5 === 0)
-        console.log(`${i}->Simplified`)
-    else {
-        console.log(`${i} -> ${i}`)
-    }
-}
+//for (let i = 1; i <= 20; ++i) {
+//    if (i % 3 === 0 && i % 5 === 0)
+//        console.log(`${i}->Frontend Simplified`)
+//    else if (i % 3 === 0)
+//        console.log(`${i}->Frontend`)
+//    else if (i % 5 === 0)
+//        console.log(`${i}->Simplified`)
+//    else {
+//        console.log(`${i} -> ${i}`)
+//    }
+//}
+
+//const str = "Frontend Simplified";
+//
+//str.split('').forEach(char => {
+//    console.log(char);
+//})
+
+
