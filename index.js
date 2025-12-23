e55f34dc32d3c177f5ad4237d5db73be62b726fa
traviscloke126 <traviscloke126@gmail.com>
2025-12-22 19:53:48 -0600

    Modal Contact Done

---
+++ index.js
@@ -0,0 +1,24 @@
+function contact(event) {
+  event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+     .sendForm(
+        'service_47bpmmu',
+        'template_ckni4uq',
+       event.target,
+        'JKEvqk1N4xxF0I6Uj'  
+    ).then(() => {
+         loading.classList.remove("modal__overlay--visible");
+         success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert("The email serivce is currently unavailable. Please contact directly at cloketravis18@gmail.com")
+    })
+}
+
+function toggleModal() {
+    
+}
